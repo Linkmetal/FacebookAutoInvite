ef049475fc4f04aa2d1b6e2520528da6bb4d7f49
Linkmetal <linkmetal18@gmail.com>
2018-04-16 15:31:47 +0200

    Commented the code a bit

--- js/background.js
+++ js/background.js
@@ -5,9 +5,6 @@ function buttonClicked(tab){
 	let msg = {
 		txt: "start"
 	}
-	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-	// 	chrome.tabs.sendMessage(tab.id, msg);
-	// });
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, msg);
 	});
--- js/content.js
+++ js/content.js
@@ -3,22 +3,21 @@
 //input type of product, and check if all have the same price
 
 
-let invited = 0;
-let posts;
-let index = 0;
+let invited = 0; //people invited
+let index = 0; //index of the actual post
+let posts; //collection of posts
 
 chrome.runtime.onMessage.addListener(gotMessage);
-console.log("lodaded");
+console.log("FacebookAutoInvite loaded.");
 function gotMessage(message, sender, sendResponse){
     if(message.txt === "start"){  
-        console.log("Entro padentro");
         posts = document.querySelectorAll("._1xnd ._2x4v");
         console.log(posts);
         openPost(index);
     }
 }
 
-function openPost(ind){
+function openPost(ind){ //open a post
     if(ind < posts.length){
         posts[ind].scrollIntoView({block: "start", behavior: "instant"});
         posts[ind].click();
@@ -27,24 +26,23 @@ function openPost(ind){
 }
 
 function autoInvite(p, a){
-    let prevLength = p;
-    let actualLength = a;
+    let prevLength = p; //previous length of likers
+    let actualLength = a; //actual length of likers
     
-    let buttons = document.querySelectorAll('._4t2a ._42ft');
+    let buttons = document.querySelectorAll('._4t2a ._42ft'); //get invite buttons
     actualLength = buttons.length;
 
     for(let i = prevLength; i < actualLength; i++){
         buttons[i].scrollIntoView({block: "start", behavior: "smooth"});
-        if(buttons[i].getAttribute('ajaxify') != null){
-            if(buttons[i].getAttribute('ajaxify').indexOf('invite') != -1){
-                buttons[i].click();
-                invited++;
-                console.log(invited);
-            }
+        if(buttons[i].getAttribute('ajaxify') != null && buttons[i].getAttribute('ajaxify').indexOf('invite') != -1){ //check if the button is an invite button and if its active
+            buttons[i].click();
+            invited++;
+            console.log(invited);
+ 
         }
     }
 
-    let seeMore = document.querySelector("#reaction_profile_pager > div > a");
+    let seeMore = document.querySelector("#reaction_profile_pager > div > a"); // get "see more" links
     if(seeMore != null){
         seeMore.click();
     }
@@ -52,11 +50,11 @@ function autoInvite(p, a){
         prevLength = actualLength;
         let buttons = document.querySelectorAll('._4t2a ._42ft');
         actualLength = buttons.length;
-        if(prevLength !== actualLength){
+        if(prevLength !== actualLength){ //check if the previous and the actual lenght is different to continue scrapping or not
             autoInvite(prevLength, actualLength);
         }
         else{
-            document.querySelector("._4t2a .layerCancel").click();
+            document.querySelector("._4t2a .layerCancel").click(); //get the close button
             window.scrollTo(0,document.body.scrollHeight);
             posts = document.querySelectorAll("._1xnd ._2x4v");
             index++;
