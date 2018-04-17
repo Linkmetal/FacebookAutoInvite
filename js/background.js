ba7524c8f1b1c04c3977606e7305b5d7b88dd69e
Linkmetal <linkmetal18@gmail.com>
2018-04-17 15:34:11 +0200

    Added div to add pages to check

--- js/background.js
+++ js/background.js
@@ -12,8 +12,8 @@ function buttonClicked(tab){
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 		  conditions: [new chrome.declarativeContent.PageStateMatcher({
-			pageUrl: {hostEquals: 'developer.chrome.com'},
-		  })
+			pageUrl: {hostEquals: 'www.facebook.com'},
+		  	})
 		  ],
 			  actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,11 @@
+if($(document).ready()){
+    var button = document.getElementById("startButton");
+    button.addEventListener('click', startScript);
+    document.getElementById("addButton").addEventListener("click", addUrl);
+    
+}
+
+
 function startScript(){
     let max = document.getElementById("maxPosts").value;
     let msg = {
@@ -9,5 +17,8 @@ function startScript(){
     });
 }
 
-var button = document.getElementById("startButton");
-button.addEventListener('click', startScript);
+function addUrl(){
+    let url = document.getElementById("newUrl").value;
+    $("#urlList").append("<p>" + url + "</p>");
+}
+
