e80ac01f6e87e18ddfd40b1f06ab91efedea3e88
Linkmetal <linkmetal18@gmail.com>
2018-04-24 15:03:39 +0200

    Added the checked comprobation for the links

--- js/popup.js
+++ js/popup.js
@@ -9,7 +9,7 @@ if($(document).ready()){
             urls = result.UrlList;
             console.log(urls);
             for(let i = 0; i < urls.length; i++){
-                $("#urlList").append("<input type='checkbox' name='urls' class='urls' checked='true'>" + urls[i] + "</input><br/>");
+                $("#urlList").append("<input type='checkbox' name='urls' class='urls' checked='true'><span>" + urls[i] + "</span></input><br/>");
             }
         }
     });
@@ -18,10 +18,17 @@ if($(document).ready()){
 
 function startScript(){
     let max = document.getElementById("maxPosts").value;
+    
+    let aux = $(".urls:checked + span");
+    let activeUrls = [];
+    for(let i = 0; i < aux.length; i++){
+        activeUrls.push($(aux[i]).text());
+    }
+    console.log(activeUrls);
     let msg = {
         txt: "start",
         maxPosts: max,
-        urlList: urls
+        urlList: activeUrls
 	}
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, msg);
@@ -33,7 +40,7 @@ function addUrl(){
     let re = new RegExp(/(https?:\/\/[^\s]+)/g);
     if(re.test(url) && urls.indexOf(url) == -1){
         urls.push(url);
-        $("#urlList").append("<input type='checkbox' name='urls' class='urls' checked='true'>" + url + "</input><br/>");
+        $("#urlList").append("<input type='checkbox' name='urls' class='urls' checked='true'><span>" + url + "</span></input><br/>");
         document.getElementById("newUrl").value = "";
         chrome.storage.sync.set({"UrlList": urls}, function() {
             console.log("Url saved");
