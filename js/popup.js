3d0aa61e1e236de0c48a68520e3922ee85b128f6
Linkmetal <linkmetal18@gmail.com>
2018-04-24 12:44:32 +0200

    Añadida persistencia para las urls

--- js/popup.js
+++ js/popup.js
@@ -4,9 +4,17 @@ if($(document).ready()){
     var button = document.getElementById("startButton");
     button.addEventListener('click', startScript);
     document.getElementById("addButton").addEventListener("click", addUrl);
-    
+    chrome.storage.sync.get(['UrlList'], function(result) {
+        if(result != null){
+            urls = result.UrlList;
+            console.log(urls);
+            for(let i = 0; i < urls.length; i++){
+                $("#urlList").append("<p>" + urls[i] + "</p>");
+            }
+        }
+    });
 }
-
+    
 
 function startScript(){
     let max = document.getElementById("maxPosts").value;
@@ -24,5 +32,8 @@ function addUrl(){
     let url = document.getElementById("newUrl").value;
     urls.push(url);
     $("#urlList").append("<p>" + url + "</p>");
+    chrome.storage.sync.set({"UrlList": urls}, function() {
+        console.log("Url saved");
+    });
 }
 
